55d1fe406f8e7d174e0f5ca25e2528967fb3e3e2
bi89 <bi1989@gmail.com>
2016-08-07 13:38:09 +0700

    Update RUN_ping_test.bat

--- RUN_ping_test.bat
+++ RUN_ping_test.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-echo READ the readme.txt!!!
+echo READ the readme.md!!!
 
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a-%%b)
